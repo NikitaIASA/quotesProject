165a892ae9a107b93bc339c1f3d94ff35ee7f27f
Nikita Chaika <iasastudent2021@gmail.com>
2023-01-06 00:39:20 +0200

    fetching data from firebase

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,17 +1,26 @@
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-
+import useHttp from "../components/hooks/use-http";
+import { addQuote } from "../components/lib/api";
 import QuoteForm from '../components/quotes/QuoteForm'
 
 export const NewQuote = () => {
+  const {sendRequest, status} = useHttp(addQuote);
   let navigate = useNavigate();
+
+  useEffect(() => {
+    if (status === 'completed') {
+      navigate('/quotes');
+    }
+  }, [status, navigate])
+
   
 
   const addQuoteHandler = quoteData => {
-    console.log(quoteData);
-    navigate('/quotes');
+    sendRequest(quoteData);
   }
 
   return (
-    <QuoteForm onAddQuote={addQuoteHandler}/>
+    <QuoteForm isLoading={status === 'pending'} onAddQuote={addQuoteHandler}/>
   )
 }
--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,24 +1,46 @@
+import { useEffect } from "react";
 import React from "react";
 import { useParams, Routes, Route, Link } from "react-router-dom";
 import Comments from "../components/comments/Comments";
 import HighlighedQuote from "../components/quotes/HighlightedQuote";
+import LoadingSpinner from '../components/UI/LoadingSpinner';
 
-const DUMMY_QUOTES = [
-  { id: "q1", author: "Max", text: "Learning React is fun!" },
-  { id: "q2", author: "Maximilian", text: "Learning React is great!" },
-];
+import useHttp from "../components/hooks/use-http";
+import { getSingleQuote } from "../components/lib/api";
 
 export const QuoteDetail = () => {
   const { quoteId } = useParams();
-  const quote = DUMMY_QUOTES.find((quote) => quote.id === quoteId);
 
-  if (!quote) {
-    return <p>No quote found</p>;
+  const {
+    sendRequest,
+    status,
+    data: loadedQuote,
+    error,
+  } = useHttp(getSingleQuote, true);
+
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
+
+  if (status === "pending") {
+    return (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <p className="centered">{error}</p>;
+  }
+
+  if (!loadedQuote.text) {
+    return <p>No quote found!</p>;
   }
 
   return (
     <>
-      <HighlighedQuote text={quote.text} author={quote.author} />
+      <HighlighedQuote text={loadedQuote.text} author={loadedQuote.author} />
       <Routes>
         <Route
           path="/"
